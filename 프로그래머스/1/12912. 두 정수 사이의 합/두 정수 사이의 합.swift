8a994460f25a815204eb847b83ffd99f892c4f69
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2024-05-17 14:23:17 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.swift
@@ -0,0 +1,15 @@
+func solution(_ a:Int, _ b:Int) -> Int64 {
+    var sum = 0
+    
+    if a < b {
+        for num in a ... b {
+            sum += num
+        }
+    } else {
+        for num in b ... a {
+            sum += num
+        }
+    }
+    
+    return Int64(sum)
+}
