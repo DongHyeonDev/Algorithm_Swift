b5c52c4d043c61bed437621ea928d2440ddbb67d
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2024-06-18 23:56:40 +0900

    [level 2] Title: H-Index, Time: 0.11 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ citations:[Int]) -> Int {
+    for (index, h) in citations.sorted(by: >).enumerated() {
+        if index >= h {
+            return index
+        }
+    }
+    
+    return citations.count
+}
